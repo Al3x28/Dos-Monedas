6b7ce1dfacf8015a556bf6e4d615988fb7a411e3
DiegoTineo <diegoatineoz@gmail.com>
2021-12-13 22:00:13 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,30 +1,11 @@
 import random 
 
-def imprimir(resultados): #recibe la lista generada por lanzarMonedas()    
-    print("RESULTADOS:")
-    i=0
-    for par in resultados:
-        i=i+1
-        print("SIMULACION " + str(i) +": " + str(par[0]) + " " + str (par[1]))
-
 def lanzarMonedas(veces):
-    resultados = [] # esta lista se utiliza para guardar los pares de lanzamientos de monedas (cada simulacion) 
-
-    for i in range (veces): #numero de simulaciones que seran realiadas
-
-        par = [] # Almacena el par de resultados.
-
-        for i in range (2): # Lanza las monedas
-            moneda = random.randint(0,1) 
-            if (moneda == 0):
-                par.append("Cara")
-            else:
-                par.append("Sello")
-
-        resultados.append(par) #añade el par a la lista de resultados                 
-
-    imprimir(resultados) 
+    posiblesResultados = ("Cara Cara", "Cara Sello", "Sello Cara", "Sello Sello") # 4 posibilidades 
+    for i in range (veces):
+        resultado = random.randint(0,3) 
+        print ("SIMULACION "+ str(i+1) +": " + posiblesResultados[resultado])
 
 if __name__ == '__main__':
-    lanzarMonedas(10)
+    lanzarMonedas(20)
     pass
